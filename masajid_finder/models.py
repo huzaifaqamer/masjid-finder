9e3fac9f4b9d558c791ef6a2d20ec461a780a3a0
Huzaifa Qamer <huzaifaqamer@gmail.com>
2018-07-08 17:44:15 +0500

    Models for masjid and contact

---
+++ masajid_finder/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=30)
+
+
+class Masjid(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=1000, blank=True, default='')
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    fajr = models.TimeField()
+    zohar = models.TimeField()
+    asr = models.TimeField()
+    maghrib = models.TimeField()
+    isha = models.TimeField()
+    jumma = models.TimeField()
+    last_modified = models.DateField(auto_now=True)
+    contact = models.ManyToManyField(Contact, null=True, blank=True)
