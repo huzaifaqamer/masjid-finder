11f1bfa542adab7d3359a555fb0b51f3bce59f0b
Huzaifa Qamer <huzaifaqamer@gmail.com>
2018-08-14 18:26:35 +0500

    mosques endpoint

--- masajid_finder/v0/urls.py
+++ masajid_finder/v0/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('mosques', views.FindMosques.as_view(), name='find-mosques'),
 ]
--- masajid_finder/v0/views.py
+++ masajid_finder/v0/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from datetime import datetime
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from masajid_finder.models import Masjid
 
 
-def index(request):
-    return HttpResponse('Hello')
+class FindMosques(APIView):
+
+    def get(self, request):
+        self.ne_lat = request.GET.get('ne_lat')
+        self.ne_lng = request.GET.get('ne_lng')
+        self.sw_lat = request.GET.get('sw_lat')
+        self.sw_lng = request.GET.get('sw_lng')
+        self.namaz = request.GET.get('namaz')
+        return Response(self.find_within_box())
+
+    def find_within_box(self):
+        current_time = datetime.time(datetime.now())
+        filters = {
+            '{0}__gt'.format(self.namaz): current_time,
+            'latitude__range': (self.sw_lat, self.ne_lat),
+            'longitude__range': (self.sw_lng, self.ne_lng)
+        }
+        return Masjid.objects.filter(**filters).values(
+                'name', 'latitude', 'longitude', self.namaz)
